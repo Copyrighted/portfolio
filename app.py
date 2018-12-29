3658b5d063ff3d600085155657b3490d9a97a089
Copyrighted <Copyrighted@users.noreply.github.com>
2018-12-29 15:29:13 -0500

    Added login page css and html

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def render_main():
     return render_template('main.html')
 
+@app.route('/notes')
+def render_notes():
+    return render_template('notes.html')
+
+@app.route('/login')
+def render_login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run()
