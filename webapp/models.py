416bbca222c36ef6838dfade0131337486a31851
tyler.scheffler@protonmail.com <tyler.scheffler@protonmail.com>
2019-05-16 14:15:41 -0700

    Added migration script/renamed app.py to blog.py

--- webapp/models.py
+++ webapp/models.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+
 from webapp import db
 
-class user(db.Model):
-    id = db.column(db.Integer, primary_key = True)
-    username = db.column(db.String(64), index= True, unique = True)
-    email = db.Column(db.String(128), index = True, unique = True)
-    password_hash = db.Column(db.String(256))
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
-
-    #repr method tells  Python how to print objects of his class
     def __repr__(self):
-        return '<user {}>'.format(self.username)
+        return '<User {}>'.format(self.username)
 
-#todo create blogpost class for db
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
