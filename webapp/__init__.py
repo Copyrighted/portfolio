7ad2ee7a4d6af68ad3dc113779b699d05580961f
tyler.scheffler@protonmail.com <tyler.scheffler@protonmail.com>
2019-05-22 19:12:12 -0700

    Added backend code for login

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from webapp.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+login = LoginManager(app)
 migrate = Migrate(app, db)
+
+
 
 from webapp import routes, models
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for, request, flash
 from webapp import app
 from webapp.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from webapp.models import User
 
 @app.route('/', methods=['GET'])
 def render_main():
@@ -12,13 +14,24 @@ def render_notes():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        #todo implement posting/editing page
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        #todo implement redirect to posting page
-        return redirect(url_for('main'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('render_login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('render_main'))
+    return render_template('login.html', title='Sign In', form=form)
 
-    return render_template('login.html', title = 'Sign In', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/secret')
