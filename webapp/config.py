e9fe152ee957b1aaa7a25ec5999bd7b2329cdf30
Copyrighted <tylerscheffler@protonmail.com>
2020-07-16 12:14:50 -0700

    Removed WTF_CSRF_TIME_LIMIT in config since I want CSRF tokens to expire, and switched Project link name in navbar to notes.  Also changed gitlab to github.

--- webapp/config.py
+++ webapp/config.py
@@ -7,4 +7,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    WTF_CSRF_TIME_LIMIT = None
